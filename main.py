c5fdca73e3abf27cdfb66bb17c974f98e81c91e4
Tomi Chen <tctree333@users.noreply.github.com>
2020-02-21 17:07:02 -0800

    switch to using the sciolyid package

---
+++ main.py
@@ -0,0 +1,34 @@
+import sciolyid as bot
+
+bot.setup(
+    bot_description = "Fossil ID - A SciOly Discord Bot for aspiring paleontologists",
+    bot_signature = "Fossil ID - A Paleontology Bot",
+    prefixes = ["f!", "f.", "F!", "F."],
+    id_type = "fossil",
+    name = "fossil-id",
+
+    github_image_repo_url = "https://github.com/tctree333/Fossil-Bot-Images.git",
+    list_dir = "lists",
+    wikipedia_file = "wikipedia.txt",
+    alias_file = "aliases.txt",
+
+    logs = True,
+    log_dir = "logs",
+    file_folder = "bot_files",
+    data_dir = "data",
+
+    local_redis = False,
+    sentry = True,
+
+    invite = "This bot is currently not avaliable outside the support server.",
+    support_server = "https://discord.gg/4KdyGc5",
+    authors = "person_v1.32, EraserBird, and hmmm",
+    source_link = "https://github.com/tctree333/Fossil-ID",
+
+    id_groups = True,
+    category_name = "Taxon",
+    category_aliases = {},
+
+)
+
+bot.start()
--- setup.sh
+++ setup.sh
@@ -5,10 +5,4 @@ echo Exported token as $token
 export ids="YOUR DISCORD USER ID"
 echo Exported ids as $ids
 
-export DISCORD_CLIENT_SECRET="YOUR CLIENT SECRET"
-echo Exported DISCORD_CLIENT_SECRET as $DISCORD_CLIENT_SECRET
-
-export GITHUB_TOKEN="YOUR GITHUB TOKEN"
-echo Exported GITHUB_TOKEN as $GITHUB_TOKEN
-
 echo Done!
