bbe2959ed15713b41c5b138a41c9cc34bd5a6712
Tomi Chen <tctree333@users.noreply.github.com>
2020-08-30 11:36:59 -0700

    add web stuff

---
+++ config.py
@@ -0,0 +1,26 @@
+config = {
+    "bot_description": "Fossil ID - A SciOly Discord Bot for aspiring paleontologists",
+    "bot_signature": "Fossil ID - A Paleontology Bot",
+    "prefixes": ["f!", "f.", "F!", "F."],
+    "id_type": "fossils",
+    "github_image_repo_url": "https://github.com/tctree333/Fossil-Bot-Images.git",
+    "support_server": "https://discord.gg/4KdyGc5",
+    "source_link": "https://github.com/tctree333/Fossil-ID",
+    "name": "fossil-id",
+
+    "category_name": "Taxon",
+
+    "client_id": 678437690848313373,
+    "base_image_url": "https://raw.githubusercontent.com/tctree333/Fossil-Bot-Images/master/",
+    "validation_repo_url": "https://github.com/person-v132/SciOlyID-Images.git",
+    "validation_repo_dir": "fossil-id",
+
+    "logs": True,
+    "bot_files_dir": "bot_files",
+    "invite": "https://discord.com/api/oauth2/authorize?client_id=678437690848313373&permissions=51200&scope=bot",
+    "authors": "person_v1.32, EraserBird, and hmmm",
+    "sentry": True,
+    "local_redis": False,
+    "backups_channel": 678435444219969538,
+    "id_groups": True,
+}
--- main.py
+++ main.py
@@ -1,35 +1,5 @@
 import sciolyid as bot
+from config import config
 
-bot.setup(
-    bot_description = "Fossil ID - A SciOly Discord Bot for aspiring paleontologists",
-    bot_signature = "Fossil ID - A Paleontology Bot",
-    prefixes = ["f!", "f.", "F!", "F."],
-    id_type = "fossils",
-    name = "fossil-id",
-
-    github_image_repo_url = "https://github.com/tctree333/Fossil-Bot-Images.git",
-    list_dir = "lists",
-    wikipedia_file = "wikipedia.txt",
-    alias_file = "aliases.txt",
-
-    logs = True,
-    log_dir = "logs",
-    bot_files_dir = "bot_files",
-    data_dir = "data",
-
-    backups_channel = 678435444219969538,
-    local_redis = False,
-    sentry = True,
-
-    invite = "https://discord.com/api/oauth2/authorize?client_id=678437690848313373&permissions=51200&scope=bot",
-    support_server = "https://discord.gg/4KdyGc5",
-    authors = "person_v1.32, EraserBird, and hmmm",
-    source_link = "https://github.com/tctree333/Fossil-ID",
-
-    id_groups = True,
-    category_name = "Taxon",
-    category_aliases = {},
-
-)
-
+bot.setup(config)
 bot.start()
