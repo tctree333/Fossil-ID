1ab89cf7575bea14222ac59e123328fafb50a2c1
Tomi Chen <tctree333@users.noreply.github.com>
2021-12-27 20:20:28 -0800

    update invite

--- config.py
+++ config.py
@@ -4,17 +4,14 @@ config = {
     "prefixes": ["f!", "f.", "F!", "F."],
     "id_type": "fossils",
     "github_image_repo_url": "https://github.com/tctree333/Fossil-Bot-Images.git",
-    "support_server": "https://discord.gg/4KdyGc5",
+    "support_server": "https://discord.gg/2HbshwGjnm",
     "source_link": "https://github.com/tctree333/Fossil-ID",
     "name": "fossil-id",
-
     "category_name": "Taxon",
-
     "client_id": 678437690848313373,
     "base_image_url": "https://raw.githubusercontent.com/tctree333/Fossil-Bot-Images/master/",
     "validation_repo_url": "https://github.com/person-v132/SciOlyID-Images.git",
     "validation_repo_dir": "fossil-id",
-
     "logs": True,
     "bot_files_dir": "bot_files",
     "invite": "https://discord.com/api/oauth2/authorize?client_id=678437690848313373&permissions=51200&scope=bot",
