488881f5c2944eb3b1e2e03840e7d76c4bb05d7c
Tomi Chen <tctree333@users.noreply.github.com>
2023-08-27 15:24:11 -0700

    don't fetch images

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-from get_images import get_images
+# from get_images import get_images
+
+async def nop(data, category, item):
+    return
 
 config = {
     "bot_description": "Fossil ID - A SciOly Discord Bot for aspiring paleontologists",
@@ -10,13 +13,13 @@ config = {
     "source_link": "https://github.com/tctree333/Fossil-ID",
     "name": "fossil-id",
     # "members_intent": False,
-    "download_func": get_images,
+    "download_func": nop, # get_images,
     "refresh_images": False,
     "evict_images": True,
     "evict_frequency": 3.0,
     "evict_threshold": 6,
     "max_evict": 2,
-    "evict_func": get_images,
+    "evict_func": nop, # get_images,
     "download_dir": "images/",
     "data_dir": "data/",
     "group_dir": "group/",
@@ -36,7 +39,7 @@ config = {
         },
         {
             "name": "Attribution",
-            "value": "Images used by this bot are the copyright of their respective owners. These images are sourced from [iDigBio](https://www.idigbio.org/).",
+            "value": "Images used by this bot are the copyright of their respective owners. These images are sourced from [iDigBio](https://www.idigbio.org/) and Wikimedia Commons.",
         },
     ],
     "id_groups": True,
